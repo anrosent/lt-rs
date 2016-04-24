1d1112800ddfee2228f4a1aff7fe629f4a7a7deb
anrosent <anson.rosenthal@gmail.com>
2016-04-24 18:28:08 -0400

    encoder commandline parsing and parameterization

--- bin/encoder.rs
+++ bin/encoder.rs
@@ -10,25 +10,50 @@ use lt::encode;
 
 fn main () {
   let args: Vec<String> = env::args().collect();
-  let exe_name = "encoder";
   let mut options = Options::new();
+  options.reqopt("f", "input_file", "File to transmit", "FILE");
+  options.reqopt("k", "num_blocks", "Number of blocks to divide the file into for transmission", "");
+  options.optopt("c", "c", "C parameter to block sampler", "");
+  options.optopt("d", "delta", "Delta parameter to block sampler", "");
+  options.optopt("s", "seed", "Seed to the block sampler PRNG", "");
+
+  // Validate args
   let matches = match options.parse(&args[1..]) {
     Ok(m) => { m },
     Err(f) => { panic!(f.to_string()) }
   };
-  let input_fn = match matches.free.len() {
-    1 => matches.free[0].clone(),
-    _ => panic!("Usage: encoder <input filename>")
-  };
-   
+
+  // Get input file from args
+  let input_fn = matches.opt_str("f").unwrap();
   let mut f = match File::open(input_fn) {
     Ok(file) => file,
     Err(e) => panic!("{}", e)
   };
-  let num_blocks = 11;
-  let params = LTBlockSamplerParams::new(num_blocks);
-  let mut encoder = encode::LTEncoder::new(params, &mut f);
+
+  // Parameterize LT sampler using options
+  let num_blocks: u32 = match matches.opt_str("k").unwrap().parse() {
+    Ok(k) => k,
+    Err(e) => panic!("Error parsing k: {}", e)
+  };
+  let mut params = LTBlockSamplerParams::new(num_blocks);
+  
+  // Add c to opts
+  params = match matches.opt_str("c") {
+    Some(c) => params.c(c.parse::<f64>().unwrap()),
+    None => params
+  };
+  // Add delta to opts
+  params = match matches.opt_str("d") {
+    Some(delta) => params.delta(delta.parse::<f64>().unwrap()),
+    None => params
+  };
+  // Add seed to opts
+  params = match matches.opt_str("s") {
+    Some(s) => params.seed(s.parse::<u32>().unwrap()),
+    None => params
+  };
+  let encoder = encode::LTEncoder::new(params, &mut f);
   for block in encoder.take(10) {
-    println!("block");
+    println!("block {:?} ", block);
   }
 }
